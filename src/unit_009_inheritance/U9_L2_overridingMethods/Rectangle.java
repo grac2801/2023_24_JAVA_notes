01c265a1dc3a450119e3f07ba6682b5c0502cab6
grac2801 <oscar.gracias@icloud.com>
2024-02-24 16:48:31 -0800

    completed U9_L2 overriding methods

---
+++ src/unit_009_inheritance/U9_L2_overridingMethods/Rectangle.java
@@ -0,0 +1,19 @@
+package unit_009_inheritance.U9_L2_overridingMethods;
+
+
+public class Rectangle extends Quadrilateral
+{
+
+    public Rectangle(int a, int b)
+    {
+        super(a, b, a, b);
+    }
+    
+    public int getPerimeter()
+    {
+        return super.getSide1() * 2 + super.getSide2() * 2;
+    }
+    
+    
+
+}
