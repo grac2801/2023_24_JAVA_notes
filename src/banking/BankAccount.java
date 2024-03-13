21353609e09ffaf8565b8e20f3cee1b100f986d9
ogracias <oscar.gracias@icloud.com>
2024-03-13 12:55:28 -0700

    set up Bank account classes

--- src/banking/BankAccount.java
+++ src/banking/BankAccount.java
@@ -2,5 +2,35 @@ package banking;
 
 public class BankAccount
 {
+    private double balance;
 
+    public BankAccount()
+    {
+        this.balance = 0;
+    }
+
+    public BankAccount(double accountBalance)
+    {
+        this.balance = accountBalance;
+    }
+
+    public void deposit(double amount)
+    {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount)
+    {
+        this.balance -= amount;
+    }
+
+    public double getBalance()
+    {
+        return this.balance;
+    }
+
+    public void setBalance(double amount)
+    {
+        this.balance = amount;
+    }
 }
